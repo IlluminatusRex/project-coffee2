a78b33497c0744f4376dad8f010f57e79e53b94b
Jakub Czesak <jakub.czesak@cbre.com>
2023-03-22 22:08:31 +0100

    Add Form html

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,101 @@
+import { settings } from './settings.js'
 const app = {
-  init: function() {
+  initPages: function(){
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    thisApp.homeLinks = document.querySelectorAll(select.nav.homelinks);
+    
+    //console.log('pages',thisApp.pages);
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[2].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        
+        // get page id from href attribute
+        const id  = clickedElement.getAttribute('href').replace('#', '');
+        
+        // run thisApp.activatePage with that id
+        thisApp.activatePage(id);
+
+        // change URL hash
+        window.location.hash = '#/' + id;
+      });
+    }
   },
-};
+
+  activatePage: function(pageId){
+    const thisApp = this;
+    // add class "active" to matching pages, remove from non-matching
+
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id==pageId);
+    }
+
+    // add class "active" to matching links, remove from non-matching
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+      
+    }
+  },
+
+  initMenu: function(){
+    const thisApp = this;
+
+    for(let productData in thisApp.data.products){
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+  },
+
+  initHome: function(){
+    const thisApp = this;
+    thisApp.homeCntr = document.querySelector(select.containerOf.home);
+    thisApp.home = new Home(thisApp.homeCntr);
+
+    thisApp.homeLinks = document.getElementById('product-list');
+    thisApp.homeLinks.addEventListener('click', function(event){
+      event.preventDefault();
+      thisApp.activatePage('products');
+    });
+
+    thisApp.homeLinks = document.getElementById('contact-wrapper');
+    thisApp.homeLinks.addEventListener('click', function(event){
+      event.preventDefault();
+      thisApp.activatePage('contact');
+    });
+
+  },
+
+    initData: function() {
+        const url = settings.db.url + '/' + settings.db.products;
+        this.data = {};
+        fetch(url)
+            .then((rawResponse) => {
+                return rawResponse.json();
+            })
+            .then((parsedResponse) => {
+                this.data.products = parsedResponse;
+            });
+    },
+
+    init: function() {
+        const thisApp = this;
+        thisApp.initPages();
+        thisApp.initData();
+        thisApp.initHome(); 
+    },
+}
 
 app.init();
--- src/js/settings.js
+++ src/js/settings.js
@@ -1,12 +1,31 @@
-export const templates = {
-  menuProduct: Handlebars.compile(document.querySelector(select.products).innerHTML),
-  home: Handlebars.compile(document.querySelector(select.templateOf.home).innerHTML),
-};
-
-export const settings = {
-  db: {
-    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
-  }
+export const select = {
+  templateOf: {
+    home: '#template-home',
+    menuProduct: '#template-products',
+    contact: '#template-contact',
+  },
+  containerOf: {
+    menu: '.producs__wrapper',
+    pages: '#pages',
+    home: '#home',
+  },
+  all: {
+    menuProducts: '#product-list > .product',
+    menuProductsActive: '#product-list > .product.active',
+  },
+  menuProduct: {
+    clickable: '.product__header',
+    form: '.product__order',
+    priceElem: '.product__total-price .price',
+    imageWrapper: '.product__images',
+    amountWidget: '.widget-amount',
+    cartButton: '[href="#add-to-cart"]',
+  },
+  
+  nav: {
+    links: '.main-nav a',
+  },
+  
 };
 
 export const classNames = {
@@ -14,26 +33,35 @@ export const classNames = {
     wrapperActive: 'active',
     imageVisible: 'active',
   },
+
+  cart: {
+    wrapperActive: 'active',
+  },
+  booking: {
+    loading: 'loading',
+    tableBooked: 'booked',
+  },
   nav: {
     active: 'active',
   },
   pages: {
     active: 'active',
   }
+
 };
 
-export const select = {
-  templateOf: {
-    home: '#template-products',
-  },
+export const settings = {
 
-  products: {
-    id: '#template-home',
-    title: '#template-menu-product',
-    description: '#template-cart-product',
-    roasting: '#template-booking-widget',
-    intensity: '#template-booking-widget',
-    popular: '#template-booking-widget',
-    image: '#template-booking-widget',
+  db: {
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
+    products: 'products',
   },
+  
+};
+
+export const templates = {
+  menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+  cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+  bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
+  home: Handlebars.compile(document.querySelector(select.templateOf.home).innerHTML),
 };

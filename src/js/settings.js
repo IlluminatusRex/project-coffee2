20827972b43421441dfd502ed1a8c6473071a3ec
Jakub Czesak <jakub.czesak@cbre.com>
2023-03-22 19:43:32 +0100

    Add app.js

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,21 +1,39 @@
 export const templates = {
-    menuProduct: Handlebars.compile(document.querySelector(select.products).innerHTML),
+  menuProduct: Handlebars.compile(document.querySelector(select.products).innerHTML),
+  home: Handlebars.compile(document.querySelector(select.templateOf.home).innerHTML),
 };
 
 export const settings = {
   db: {
     url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
   }
-}
+};
+
+export const classNames = {
+  menuProduct: {
+    wrapperActive: 'active',
+    imageVisible: 'active',
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  }
+};
 
 export const select = {
-    products: {
-      id: '#template-home',
-      title: '#template-menu-product',
-      description: '#template-cart-product',
-      roasting: '#template-booking-widget',
-      intensity: '#template-booking-widget',
-      popular: '#template-booking-widget',
-      image: '#template-booking-widget',
-    },
-}
+  templateOf: {
+    home: '#template-products',
+  },
+
+  products: {
+    id: '#template-home',
+    title: '#template-menu-product',
+    description: '#template-cart-product',
+    roasting: '#template-booking-widget',
+    intensity: '#template-booking-widget',
+    popular: '#template-booking-widget',
+    image: '#template-booking-widget',
+  },
+};
